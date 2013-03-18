e79df645d9d95b088ad138c4b1f1ee0199940040
lahabana (Charly MOLTER) <charly.molter@gmail.com>
2013-03-18 17:19:18 +0100

    adding just a basic hello world with maven params etc

---
+++ src/main/java/com/ntnu/tdtd4300/App.java
@@ -0,0 +1,15 @@
+package com.ntnu.tdtd4300;
+
+import org.apache.lucene.store.Directory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
