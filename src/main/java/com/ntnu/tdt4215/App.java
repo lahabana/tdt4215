e40287518f50c31a2813f559c45d19955b803dcb
lahabana (Charly MOLTER) <charly.molter@gmail.com>
2013-03-21 15:46:03 +0100

    adding a main for the command line

--- src/main/java/com/ntnu/tdt4215/App.java
+++ src/main/java/com/ntnu/tdt4215/App.java
@@ -1,78 +1,88 @@
 package com.ntnu.tdt4215;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopScoreDocCollector;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.Version;
 
+import com.ntnu.tdt4215.index.DirectoryManager;
+import com.ntnu.tdt4215.query.QueryFactory;
+import com.ntnu.tdt4215.query.SimpleQueryFactory;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Vector;
 
 public class App {
-  public static void main(String[] args) throws IOException, ParseException {
-    // 0. Specify the analyzer for tokenizing text.
-    //    The same analyzer should be used for indexing and searching
-    StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_40);
-
-    // 1. create the index
-    Directory index = new RAMDirectory();
-
-    IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_40, analyzer);
-
-    IndexWriter w = new IndexWriter(index, config);
-    addDoc(w, "Lucene in Action", "193398817");
-    addDoc(w, "Lucene for Dummies", "55320055Z");
-    addDoc(w, "Managing Gigabytes", "55063554A");
-    addDoc(w, "The Art of Computer Science", "9900333X");
-    w.close();
-
-    // 2. query
-    String querystr = args.length > 0 ? args[0] : "lucene";
-
-    // the "title" arg specifies the default field to use
-    // when no field is explicitly specified in the query.
-    Query q = new QueryParser(Version.LUCENE_40, "title", analyzer).parse(querystr);
-
-    // 3. search
-    int hitsPerPage = 10;
-    IndexReader reader = DirectoryReader.open(index);
-    IndexSearcher searcher = new IndexSearcher(reader);
-    TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
-    searcher.search(q, collector);
-    ScoreDoc[] hits = collector.topDocs().scoreDocs;
-   
-    // 4. display results
-    System.out.println("Found " + hits.length + " hits.");
-    for(int i=0;i<hits.length;++i) {
-      int docId = hits[i].doc;
-      Document d = searcher.doc(docId);
-      System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+	
+	static Directory index;
+	static StandardAnalyzer analyzer;
+	static QueryFactory qpf;
+	static DirectoryManager manager;
+	final static File FILE = new File("bingou");
+	
+	public static void main(String[] args) throws IOException, ParseException {
+		// We clean the folder containing the index
+    	if (args.length == 1 && args[0].equals("--clean")) {
+    		if (FILE.exists() && FILE.isDirectory() && FILE.canWrite()) {
+    			FileUtils.deleteDirectory(FILE);
+    		} else {
+    			System.err.println("Can't delete the directory:" + FILE.getAbsolutePath());
+    			System.exit(-1);
+    		}
+    		return;
+    	}
+    	// We measure the performances
+    	if (args.length == 1 && args[0].equals("--measure")) {
+    		initIndex();
+    		return;
+		}
+    	// We want to index new documents
+    	if (args.length == 1 && args[0].equals("--index")) {
+    		initIndex();
+    		// Create your fsm
+    		//manager.addAll(fsm);
+    		return;
+    	}
+    	// We want to search the index
+    	if(args.length == 2 && args[0].equals("--search")) {
+    		initIndex();
+    		Vector<Document> docs = manager.getResults(3, args[1]);
+    		for(Document d: docs) {
+    			System.out.println("Match:" + d.get("title"));
+    		}
+    		return;
+    	} 
+    	showHelp();
     }
+	
+	private static void initIndex() throws IOException {
+	    // Create the folder that will hold the index on the FS
+		if (!FILE.exists()) {
+	    	if (!FILE.mkdir()) {
+    			System.err.println("Can't create the directory:" + FILE.getAbsolutePath() + 
+    								" to save the index");
+    			System.exit(-1);	    		
+	    	}
+	    }
+		index = new SimpleFSDirectory(FILE);
+	
+	    qpf = new SimpleQueryFactory();
+	    analyzer = new StandardAnalyzer(Version.LUCENE_40);
+	    manager = new DirectoryManager(index, analyzer, qpf);
+	}
 
-    // reader can only be closed when there
-    // is no need to access the documents any more.
-    reader.close();
-  }
-
-  private static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
-    Document doc = new Document();
-    doc.add(new TextField("title", title, Field.Store.YES));
-
-    // use a string field for isbn because we don't want it tokenized
-    doc.add(new StringField("isbn", isbn, Field.Store.YES));
-    w.addDocument(doc);
-  }
+	private static void showHelp() {
+		System.out.println("-------TDT 4215 project app usage-----");
+		System.out.println("By: Anne-Sophie Gourlay, David Katuščák and Charly Molter");
+		System.out.println("\tOptions:");
+		System.out.println("\t\t--index: index documents");
+		System.out.println("\t\t--search <query>: search the index for a match query");
+		System.out.println("\t\t--clean: empty the index");
+		System.out.println("\t\t--measure: compare to the gold standard and output some stats");
+	
+	}
 }
