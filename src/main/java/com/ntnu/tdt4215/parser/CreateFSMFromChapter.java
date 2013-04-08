6a0352d62ace07bf1049bd4ff7fec22141cad669
huzvak <katuscak.d@gmail.com>
2013-04-09 00:18:06 +0200

    New version of parser

--- src/main/java/com/ntnu/tdt4215/parser/CreateFSMFromChapter.java
+++ src/main/java/com/ntnu/tdt4215/parser/CreateFSMFromChapter.java
@@ -1,8 +1,9 @@
 package com.ntnu.tdt4215.parser;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -22,148 +23,177 @@ public class CreateFSMFromChapter {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		CreateFSMFromChapter createChapters = new CreateFSMFromChapter();
-		
+
 		ArrayList<NLHChapter> NLHChapters = new ArrayList<NLHChapter>();
-		
-		ArrayList<String> chaptersG = null;
-		ArrayList<String> chaptersL = null;
-		ArrayList<String> chaptersT = null;
-		
-		chaptersG = createChapters.getURLAddressesToChapters("Download/G/innhold.htm");
-		chaptersL = createChapters.getURLAddressesToChapters("Download/L/innhold.htm");
-		chaptersT = createChapters.getURLAddressesToChapters("Download/T/innhold.htm");
-		
+
+		ArrayList<File> dirG = createChapters
+				.getFilesInDirectory("Download/G/");
+		ArrayList<File> dirL = createChapters
+				.getFilesInDirectory("Download/L/");
+		ArrayList<File> dirT = createChapters
+				.getFilesInDirectory("Download/T/");
+
 		int i = 0;
-		
-		for(String g : chaptersG) {
-			HashSet<String> contentSet = createChapters.getContentFromURLAddress("Download/G/" + g);
-			createChapters.createNLHChapter("Download/G/" + g, contentSet, NLHChapters);
+
+		for (File f : dirG) {
+			createChapters.parseFile("Download/G/" + f.getName(), NLHChapters);
 		}
-		
-		for(String g : chaptersL) {
-			HashSet<String> contentSet = createChapters.getContentFromURLAddress("Download/L/" + g);
-			createChapters.createNLHChapter("Download/L/" + g, contentSet, NLHChapters);
+
+		for (File f : dirL) {
+			createChapters.parseFile("Download/L/" + f.getName(), NLHChapters);
 		}
-		
-//		for(String g : chaptersT) {
-////			i++;
-////			if(i < 2) {
-//			HashSet<String> contentSet = createChapters.getContentFromURLAddress("Download/T/" + g);
-//			createChapters.createNLHChapter("Download/T/" + g, contentSet, NLHChapters);
-////			}
-//		}
-		
+
+		for (File f : dirT) {
+			createChapters.parseFile("Download/T/" + f.getName(), NLHChapters);
+		}
+
 		System.out.println("Size of chapters: " + NLHChapters.size());
-		
-		for(NLHChapter c : NLHChapters) {
-			System.out.println("Chapter: " + c.getDocument());
+
+		for (NLHChapter c : NLHChapters) {
+			i++;
+			if (i < 6) {
+				System.out.println("Chapter: " + c.getDocument());
+			}
 		}
 	}
-	
-	private void createNLHChapter(String address, HashSet<String> contentSet, ArrayList<NLHChapter> NLHChapters) {
+
+	private ArrayList<File> getFilesInDirectory(String dirName) {
+		ArrayList<File> files = new ArrayList<File>();
+
+		File dir = new File(dirName);
+
+		File[] temp = dir.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String filename) {
+				return filename.endsWith(".htm");
+			}
+		});
+
+		for (File f : temp) {
+			if (!(f.getName().equals("innhold.htm"))
+					&& !(f.getName().equals(".htm"))) {
+				files.add(f);
+			}
+		}
+
+		return files;
+	}
+
+	private void parseFile(String address, ArrayList<NLHChapter> NLHChapters) {
 		FileInputStream fis = null;
 		BufferedReader reader = null;
-        try {
-            fis = new FileInputStream(address);
-            reader = new BufferedReader(new InputStreamReader(fis));
-            
-            String line = reader.readLine();
-            
-            while(line != null) {
-            	
-            	if(line.contains("<h1  ")) {
-            		Pattern pattern = Pattern.compile("^<h1.*\">(.*)</h.*");
-            		
-            		Matcher matcher = pattern.matcher(line);
-            	    while (matcher.find()) {
-            	    	String headLine = matcher.group(1);
-            	    	
-            	    	String[] parts = headLine.split("&nbsp;");
-            	    	
-            	    	NLHChapters.add(new NLHChapter(parts[0], parts[1], contentSet.toString()));
-            	    }
-            		
-            	}
-              line = reader.readLine();
-          }
-        } catch (IOException ex) {
-            Logger.getLogger(CreateFSMFromChapter.class.getName()).log(Level.SEVERE, null, ex);
-          
-        } finally {
-            try {
-                fis.close();
-                reader.close();
-            } catch (IOException ex) {
-                Logger.getLogger(CreateFSMFromChapter.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-	}
-	
-	private HashSet<String> getContentFromURLAddress(String address) {
-		FileInputStream fis = null;
-		HashSet<String> contentSet = new HashSet<String>();
-        
-        try {
-            fis = new FileInputStream(address);
-            
-            String text = Jsoup.parse(fis, "UTF-8", address).text();
-            String[] temp = text.split(" ");
-            
-            for(String t: temp) {
-            	contentSet.add(t);
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(CreateFSMFromChapter.class.getName()).log(Level.SEVERE, null, ex);
-          
-        } finally {
-            try {
-                fis.close();
-            } catch (IOException ex) {
-                Logger.getLogger(CreateFSMFromChapter.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        
-        return contentSet;
-	}
-	
-	private ArrayList<String> getURLAddressesToChapters(String address) {
-		FileInputStream fis = null;
-        BufferedReader reader = null;
-        
-        ArrayList<String> chapters = new ArrayList<String>();
-      
-        try {
-            fis = new FileInputStream(address);
-            reader = new BufferedReader(new InputStreamReader(fis));
-          
-            String line = reader.readLine();
-            while(line != null){
-            	
-            	if(line.contains("<a class=\"menya\" href=\"")) {
-            		String[] temp = line.split("href=\"");
-            		String[] temp2 = temp[1].split("\"");
-            		
-            		chapters.add(temp2[0]);
-            	}
-                line = reader.readLine();
-            }           
-          
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(CreateFSMFromChapter.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(CreateFSMFromChapter.class.getName()).log(Level.SEVERE, null, ex);
-          
-        } finally {
-            try {
-                reader.close();
-                fis.close();
-            } catch (IOException ex) {
-                Logger.getLogger(CreateFSMFromChapter.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        
-        return chapters;
+		try {
+			fis = new FileInputStream(address);
+			reader = new BufferedReader(new InputStreamReader(fis));
+
+			String line = reader.readLine();
+
+			String[] parts = new String[2];
+			HashSet<String> chapterContentSet = new HashSet<String>();
+
+			boolean newChapter = false;
+
+			while (line != null) {
+
+				if (line.contains("<h1  ") || line.contains("<h2  ")
+						|| line.contains("<h3  ") || line.contains("<h4  ")) {
+
+					if (!(chapterContentSet.isEmpty()) && parts != null
+							&& newChapter) {
+
+						NLHChapters.add(new NLHChapter(parts[0], parts[1],
+								chapterContentSet.toString()));
+						parts = null;
+						chapterContentSet = new HashSet<String>();
+						newChapter = false;
+					}
+				}
+
+				if (line.contains("<h1  ")) {
+					Pattern pattern = Pattern.compile(".*<h1.*\\\">(.*)</h.*");
+
+					Matcher matcher = pattern.matcher(line);
+					while (matcher.find()) {
+						String headLine = matcher.group(1);
+
+						parts = headLine.split("&nbsp;");
+
+						if (parts.length == 2) {
+							newChapter = true;
+						}
+					}
+				} else if (line.contains("<h2  ")) {
+					Pattern pattern = Pattern.compile(".*<h2.*\\\">(.*)</h.*");
+
+					Matcher matcher = pattern.matcher(line);
+					while (matcher.find()) {
+						String headLine = matcher.group(1);
+
+						parts = headLine.split("&nbsp;");
+
+						if (parts.length == 2) {
+							newChapter = true;
+						}
+					}
+				} else if (line.contains("<h3  ")) {
+					Pattern pattern = Pattern.compile(".*<h3.*\\\">(.*)</h.*");
+
+					Matcher matcher = pattern.matcher(line);
+					while (matcher.find()) {
+						String headLine = matcher.group(1);
+
+						parts = headLine.split("&nbsp;");
+
+						if (parts.length == 2) {
+							newChapter = true;
+						}
+					}
+				} else if (line.contains("<h4  ")) {
+					Pattern pattern = Pattern.compile(".*<h4.*\\\">(.*)</h.*");
+
+					Matcher matcher = pattern.matcher(line);
+					while (matcher.find()) {
+						String headLine = matcher.group(1);
+
+						parts = headLine.split("&nbsp;");
+
+						if (parts.length == 2) {
+							newChapter = true;
+						}
+					}
+				} else {
+					String text = Jsoup.parse(line).text();
+					String[] temp = text.split(" ");
+
+					for (String t : temp) {
+						chapterContentSet.add(t);
+					}
+				}
+				line = reader.readLine();
+			}
+
+			if (!(chapterContentSet.isEmpty()) && parts != null && newChapter) {
+
+				NLHChapters.add(new NLHChapter(parts[0], parts[1],
+						chapterContentSet.toString()));
+				parts = null;
+				chapterContentSet = new HashSet<String>();
+				newChapter = false;
+			}
+
+		} catch (IOException ex) {
+			Logger.getLogger(CreateFSMFromChapter.class.getName()).log(
+					Level.SEVERE, null, ex);
+
+		} finally {
+			try {
+				fis.close();
+				reader.close();
+			} catch (IOException ex) {
+				Logger.getLogger(CreateFSMFromChapter.class.getName()).log(
+						Level.SEVERE, null, ex);
+			}
+		}
 	}
 }
