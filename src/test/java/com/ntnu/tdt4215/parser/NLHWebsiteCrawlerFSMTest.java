e488636feaec7ba32d575c05ebf718ec60135f02
lahabana (Charly MOLTER) <charly.molter@gmail.com>
2013-04-09 19:12:30 +0200

    adding a test folder and some basic test

---
+++ src/test/java/com/ntnu/tdt4215/parser/NLHWebsiteCrawlerFSMTest.java
@@ -0,0 +1,41 @@
+package com.ntnu.tdt4215.parser;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.ntnu.tdt4215.document.NLHChapter;
+import com.ntnu.tdt4215.parser.NLHWebsiteCrawlerFSM;
+
+
+public class NLHWebsiteCrawlerFSMTest {
+
+	//@Test
+	@Ignore
+	public void ListTest() {
+		String[] folders = {"Download/G/", "Download/L/", "Download/T/"};
+		NLHWebsiteCrawlerFSM fsm = new NLHWebsiteCrawlerFSM(folders);
+		for (File f: fsm.files) {
+			System.out.println(f.getPath());
+		}
+		assertTrue(true);
+	}
+	
+	@Test
+	public void ExtractChaptersSelectors() {
+		String[] folders = {"TestChapters/"};
+		NLHWebsiteCrawlerFSM fsm = new NLHWebsiteCrawlerFSM(folders);
+		fsm.initialize();
+		while (fsm.hasNext()) {
+			NLHChapter ch = fsm.next();
+			System.out.println(ch.getTitle());
+		}
+		fsm.finish();
+		assertTrue(true);
+	}
+	
+	
+}
