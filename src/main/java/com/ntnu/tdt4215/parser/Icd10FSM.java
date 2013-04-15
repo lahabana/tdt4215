f58d52eb9b063dccbab6768ac493e8042ffbab7d
lahabana (Charly MOLTER) <charly.molter@gmail.com>
2013-04-15 18:06:03 +0200

    removing all non final icd entries from the index

--- src/main/java/com/ntnu/tdt4215/parser/Icd10FSM.java
+++ src/main/java/com/ntnu/tdt4215/parser/Icd10FSM.java
@@ -1,6 +1,9 @@
 package com.ntnu.tdt4215.parser;
 
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.Selector;
 import com.hp.hpl.jena.rdf.model.SimpleSelector;
 import com.hp.hpl.jena.rdf.model.Statement;
@@ -13,13 +16,23 @@ import com.ntnu.tdt4215.document.Icd10Class;
  *
  */
 public class Icd10FSM extends OwlFSM {
+	static String ns = "http://research.idi.ntnu.no/hilab/ehr/ontologies/icd10no.owl#";
+	static Property code_compacted = ResourceFactory.createProperty(ns + "code_compacted");
+	
 	/**
 	 * Our selector to select only the classes
 	 */
 	protected static Selector stSelector = new SimpleSelector(null, null, (RDFNode) null) {
         public boolean selects(Statement s) {
         	if (s.getObject().equals(OWL.Class)) {
-    			return true;
+        		Resource  subject   = s.getSubject();
+        		if (subject.hasProperty(code_compacted)) {
+        		    Statement codeStmt = subject.getProperty(code_compacted);
+        		    if (codeStmt.getString().length() != 0 &&
+        		    	!codeStmt.getString().contains("-")) {
+        		    	return true;
+        		    } 	
+        		}
     		}
         	return false;
         }
