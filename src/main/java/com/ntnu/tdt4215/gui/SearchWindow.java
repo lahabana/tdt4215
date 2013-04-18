0e345cd5f7662839769ded98ed40b279feaa1f2d
lahabana (Charly MOLTER) <charly.molter@gmail.com>
2013-04-18 14:07:20 +0200

    better GUI

--- src/main/java/com/ntnu/tdt4215/gui/SearchWindow.java
+++ src/main/java/com/ntnu/tdt4215/gui/SearchWindow.java
@@ -1,12 +1,16 @@
 package com.ntnu.tdt4215.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Collection;
 import java.util.Iterator;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -28,53 +32,55 @@ public class SearchWindow extends JFrame {
 	JTextArea searchSpace;
 	JPanel results = null;
 	private JTextField nbRes;
-	private JPanel mainPanel;
 
 	public SearchWindow(SearchEngine se) {
 		this.searchEngine = se;
+		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 		
-		mainPanel = new JPanel(new GridLayout(0, 1));
-		
-		JPanel topPanel = new JPanel(new GridLayout(0, 4));
+		JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEADING));
 		JLabel labelnbResults = new JLabel("How many results:");
-		nbRes = new JTextField("5");
+		nbRes = new JTextField("   5");
 		topPanel.add(labelnbResults);
 		topPanel.add(nbRes);
-		mainPanel.add(topPanel);
+		topPanel.setAlignmentX(0);
+		add(topPanel);
+
 		
 		JLabel label = new JLabel("Your search here:");
-		mainPanel.add(label);
+		label.setAlignmentX(0);
+		add(label);
 		
 		searchSpace = new JTextArea();
-		searchSpace.setSize(400, 200);
-		mainPanel.add(searchSpace);
+		searchSpace.setAlignmentX(0);
+		add(searchSpace);
 		
 		button = new JButton("Search");
 		button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	if (results != null) {
-    				mainPanel.remove(results);
+    				remove(results);
     				results = null;
     			}
-            	int maxResults = Integer.parseInt(nbRes.getText());
+            	int maxResults = Integer.parseInt(nbRes.getText().trim());
             	String query = searchSpace.getText();
             	search(maxResults, query);
             }
         });
-		mainPanel.add(button);
+		button.setAlignmentX(0);
+		add(button);
 		
 		JLabel labelRes = new JLabel("Results:");
-		mainPanel.add(labelRes);
-		add(mainPanel);
+		labelRes.setAlignmentX(0);
+		add(labelRes);
 		setVisible(true);
-		setSize(mainPanel.getPreferredSize());
+		setSize(500, 500);
 	}
 	
 	void search(int maxResults, String queryString) {
 		try {
 			Collection<ScoredDocument> res = searchEngine.getResults(maxResults, queryString);
 			results = new JPanel();
-			results.setLayout(new GridLayout(0,2));
+			results.setLayout(new GridLayout(0, 2));
 	        
 	        Iterator<ScoredDocument> it = res.iterator();
 	        for (int i = 0; i < maxResults && it.hasNext(); i++) {
@@ -82,9 +88,10 @@ public class SearchWindow extends JFrame {
 	        	results.add(new JLabel(Float.toString(doc.getScore())));
 	        	results.add(new JLabel(doc.getField("title")));
 	        }
-	        mainPanel.add(results);
-	        mainPanel.validate();
-	        mainPanel.repaint();
+	        results.setAlignmentX(0);
+	        add(results);
+	        validate();
+	        repaint();
 		} catch (Exception e) {
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(this,
